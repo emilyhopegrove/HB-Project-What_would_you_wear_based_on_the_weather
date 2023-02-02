434a6396acd0fb6b6dcef76c48a763e87f2b125f
Emily Grove <emilyhopegrove@gmail.com>
2023-02-01 19:17:22 -0600

    re-initial commit

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
 # practice
 import requests
-
+import os
 # user_home_zip = input("Enter your home zip: ")
 # user_work_zip = input("Enter your work zip: ")
 # user_other_zip = input("Enter another favorite zip: ")
 
 geocode_url = 'http://api.openweathermap.org/geo/1.0/zip'
 
-api_key = '437ebc27db78460bc96c720884188dbb'
+api_key = os.environ['api_key']
 
 geocode_params = {
     'zip': 78703,
